45e34276c0289c8876c3014b132c613d89121e3d
catslanderer <45492869+catslanderer@users.noreply.github.com>
2019-07-10 16:20:52 -0400

    Modified /mine to receive and validate or reject proof. Rejection works–not ready to test valid proof.

--- client_mining_p/blockchain.py
+++ client_mining_p/blockchain.py
@@ -148,33 +148,41 @@ def last_proof():
     return jsonify(response), 200
 
 
-# @app.route('/mine', methods=['GET'])
-# def mine():
-#     # We run the proof of work algorithm to get the next proof...
-#     last_block = blockchain.last_block
-#     last_proof = last_block['proof']
-#     proof = blockchain.proof_of_work(last_proof)
+@app.route('/mine', methods=['POST'])
+def mine():
+    # request should be an object containing 'sender' and 'proof' properties
+    print(request.get_json())
+    sender = request.get_json()['sender']
+    proof = request.get_json()['proof']
+    # We run the proof of work algorithm to get the next proof...
+    last_block = blockchain.last_block
+    last_proof = last_block['proof']
 
-#     # We must receive a reward for finding the proof.
-#     # The sender is "0" to signify that this node has mine a new coin
-#     blockchain.new_transaction(
-#         sender="0",
-#         recipient=node_identifier,
-#         amount=1,
-#     )
+    if blockchain.valid_proof(last_proof, proof) is True:
+        # We must receive a reward for finding the proof.
+        # The sender is "0" to signify that this node has mined a new coin
+        blockchain.new_transaction(sender, sender, 1)
 
-#     # Forge the new BLock by adding it to the chain
-#     previous_hash = blockchain.hash(last_block)
-#     block = blockchain.new_block(proof, previous_hash)
+        # Forge the new BLock by adding it to the chain
+        previous_hash = blockchain.hash(last_block)
+        block = blockchain.new_block(proof, previous_hash)
 
-#     response = {
-#         'message': "New Block Forged",
-#         'index': block['index'],
-#         'transactions': block['transactions'],
-#         'proof': block['proof'],
-#         'previous_hash': block['previous_hash'],
-#     }
-#     return jsonify(response), 200
+        response = {
+            'message': "New Block Forged",
+            'index': block['index'],
+            'transactions': block['transactions'],
+            'proof': block['proof'],
+            'previous_hash': block['previous_hash'],
+        }
+
+        return jsonify(response), 200
+
+    else:
+        response = {
+            'message': "Proof of Work Rejected"
+        }
+
+        return jsonify(response), 200
 
 
 @app.route('/transactions/new', methods=['POST'])
